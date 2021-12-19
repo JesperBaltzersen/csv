291c68ad5a67270e279d5b98ad56f635e2e33e20
jespe <jespe@JB>
2021-12-19 17:33:06 +0100

    Add project files.

---
+++ WebApplication1/Controllers/test.fs
@@ -0,0 +1,31 @@
+﻿namespace WebApplication1.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+open WebApplication1
+open FSharp.Data
+open System.Text.Json
+
+[<ApiController>]
+[<Route("[controller]")>]
+type testController (logger: ILogger<testController>) = 
+    inherit ControllerBase()
+
+    let returnList = 
+        [|
+            "test"
+            "test2"
+        |]
+
+    let human = { FullName = "Jesper Baltzersen" ; Age = 21}
+
+    [<HttpGet>]
+    member _.Get() =
+        let data = JsonSerializer.Serialize human
+        data
+
+            
